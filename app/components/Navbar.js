4d5fb465dbd88bad55340fdd586bb93178409ce1
kpm12 <kpm12@my.fsu.edu>
2017-06-19 23:42:22 -0400

    added navbar

---
+++ app/components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    var now = new Date();
+
+    return(
+      <nav className='navbar navbar-default navbar-static-top'>
+        <div className='navbar-header'>
+          <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar'>
+            <span className='sr-only'>Toggle navigation</span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+            <span className='icon-bar'></span>
+          </button>
+          <Link to='/' className='navbar-brand'>
+            Hollerith
+          </Link>
+        </div>
+        <div id='navbar' className='navbar-collapse collapse'>
+          <ul className='nav navbar-nav'>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/stats'>Stats</Link></li>
+            <li><Link to='/add'>Add</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
